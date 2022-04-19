e3c1894e5b01a922df6d4b32536c2df2a7b60b55
Sagat <sagatfarhat@gmail.com>
2022-04-20 01:19:15 +0600

    22w8a

--- routes/fifth_pizza.js
+++ routes/fifth_pizza.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 router
     .route("/")
-    .get((req, res) => res.render(__dirname + "/views/Example.ejs"))
+    .get((req, res) => res.render(__dirname + "/views/Example.ejs", {pizzaName: "Marinara"}))
     .post((req, res) => res.send("POST"));
 module.exports = router;
