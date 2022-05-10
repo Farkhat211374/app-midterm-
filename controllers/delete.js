3cb85fe0c993c22936dc915a5d9675bfa04ec61d
1Harm <71336525+1Harm@users.noreply.github.com>
2022-05-10 19:16:01 +0600

    Add files via upload

---
+++ controllers/delete.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+let db = mongoose.connection;
+
+module.exports = async function deleteUser(req, res){
+    var email = req.body.email;
+    var data = {
+        "email": email
+    }
+    await db.collection('Harm').deleteOne(data, function (err, collection) {
+        if (err) throw err;
+        console.log("Record deleted Successfully");
+
+    });
+    console.log("server listening at port 7777");
+}
