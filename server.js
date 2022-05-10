84ea9722e57469a037c84e5ad0be49ddbf84423a
1Harm <71336525+1Harm@users.noreply.github.com>
2022-05-10 19:16:27 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -2,24 +2,20 @@ const  express = require('express')
 const app = express()
 const port = 7777
 app.set('view engine','ejs')
-app.use("/", require("./routes/root"));
-app.use("/register", require("./routes/register"));
-app.use("/log", require("./routes/log"));
-app.use("/tours", require("./routes/tours"));
-app.use("/service", require("./routes/service"));
-app.use("/contacts", require("./routes/contacts"));
-app.use("/first_pizza",require("./routes/first_pizza"));
-app.use("/second_pizza",require("./routes/second_pizza"));
-app.use("/buy",require("./routes/buy"));
-app.use("/third_pizza",require("./routes/third_pizza"));
-app.use("/fourth_pizza",require("./routes/fourth_pizza"));
-app.use("/fifth_pizza",require("./routes/fifth_pizza"));
-app.use("/sixth_pizza",require("./routes/sixth_pizza"));
-app.use("/sale",require("./routes/sale"));
-app.use("/reg",require("./routes/reg"));
-
-
-
+app.use("/", require("./root"));
+app.use("/register", require("./register"));
+app.use("/tours", require("./tours"));
+app.use("/service", require("./service"));
+app.use("/contacts", require("./contacts"));
+app.use("/first_pizza",require("./first_pizza"));
+app.use("/second_pizza",require("./second_pizza"));
+app.use("/buy",require("./buy"));
+app.use("/third_pizza",require("./third_pizza"));
+app.use("/fourth_pizza",require("./fourth_pizza"));
+app.use("/fifth_pizza",require("./fifth_pizza"));
+app.use("/sixth_pizza",require("./sixth_pizza"));
+app.use("/sale",require("./sale"));
+app.use("/reg",require("./reg"));
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
 );
@@ -29,6 +25,7 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://1Harm:EsOd9029@cluster0.7hrdf.mongodb.net/Signin');
 let db = mongoose.connection;
+
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
     console.log("connection succeeded");
@@ -38,35 +35,29 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use("/", require("./log"))
+app.use("/register", require('./log'))
 
-app.post('/sign_up', function(req,res){
-    let name = req.body.name;
-    let email = req.body.email;
-    let pass = req.body.password;
-    let phone = req.body.phone;
+app.post('/sign_up', function(req,res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var pass = req.body.password;
+    var phone = req.body.phone;
 
-    let data = {
+    var data = {
         "name": name,
         "email": email,
         "password": pass,
         "phone": phone
-    };
-    db.collection('Harm').insertOne(data,function(err, collection){
+    }
+    db.collection('Harm').insertOne(data, function (err, collection) {
         if (err) throw err;
         console.log("Record inserted Successfully");
 
     });
-
-    return res.redirect('Register.ejs');
+    console.log("server listening at port 7777");
 })
-
-
-app.get('/',function(req,res){
-    res.set({
-        'Access-control-Allow-Origin': '*'
-    });
-    return res.redirect('Register.ejs');
-}).listen(3000)
-
-
-console.log("server listening at port 7777");
+app.get('/getUsers', function(req,res) {
+        const users = db.collection('Harm').find()
+        console.log(res.send(users))
+})
