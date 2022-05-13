d43a68690da61330525c06512c0aff0dbfd5c27d
1Harm <71336525+1Harm@users.noreply.github.com>
2022-05-13 19:16:06 +0600

    Add files via upload

--- routes/log.js
+++ routes/log.js
@@ -1,16 +1,18 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 const {check} = require('express-validator')
-const path = require("path");
 const mongoose = require("mongoose");
 let db = mongoose.connection;
 const deleteCtrl = require('../controllers/delete')
 const getUsersCtrl = require('../controllers/getUsers')
+const UserController = require('../controllers/UserController')
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
     console.log("connection succeeded");
 })
 router.get('/log', (req,res) => res.render(path.resolve("routes/views/Register.ejs")))
-
 router.post('/delete', deleteCtrl)
+router.post('/update', UserController.update);
+router.get('/getUsers', getUsersCtrl)
 module.exports = router
